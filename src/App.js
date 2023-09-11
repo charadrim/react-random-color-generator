7e5e1dcea31e2502a204ae077e2316e22d3953fb
Chara Drimiskianaki <charadrim@yahoo.gr>
2023-09-11 22:25:15 +0300

    Finish react random color generator app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import randomColor from 'randomcolor';
 import { useState } from 'react';
-import { HexColorPicker } from 'react-colorful';
 
 export default function App() {
   const initialColor = randomColor();
   const [hexColor, setHexColor] = useState(initialColor);
-  const colorChange = (newColor) => {
-    setHexColor(newColor);
-  };
   return (
     <>
       <h1>Random Color Generator</h1>
