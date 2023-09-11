bf8d9c5038ac943fca04da0e06ac8982d9c47a11
Chara Drimiskianaki <charadrim@yahoo.gr>
2023-09-11 21:44:23 +0300

    Finish random color generator app React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import './App.css';
-import logo from './logo.svg';
+/*Design and develop a random color generator web application using React. This application will show a random color when a button is clicked:
+
+-A button with the exact text Generate will cause a new color to be generated when it is clicked
+-Once a color is generated, update the background color of a div which contains the exact text Generated Color: <background color hex code> (the background color and the hex code must match)*/
+
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+import { HexColorPicker } from 'react-colorful';
 
 export default function App() {
+  const initialColor = randomColor();
+  const [hexColor, setHexColor] = useState(initialColor);
+  const colorChange = (newColor) => {
+    setHexColor(newColor);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Random Color Generator</h1>
+      <div
+        style={{
+          fontSize: '50px',
+          color: hexColor,
+        }}
+      >
+        {hexColor}
+      </div>
+      <br />
+      <button onClick={() => setHexColor(randomColor())}>Generate</button>
+    </>
   );
 }
