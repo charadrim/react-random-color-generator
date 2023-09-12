63eac64778f19be76f93a8e914d02d69dfdbde3d
Chara Drimiskianaki <charadrim@yahoo.gr>
2023-09-12 09:43:16 +0300

    Finish react random color generator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import randomColor from 'randomcolor';
 import { useState } from 'react';
+import tinycolor from 'tinycolor2';
 
 export default function App() {
   const initialColor = randomColor();
   const [hexColor, setHexColor] = useState(initialColor);
+  const textColor = tinycolor(hexColor).isLight() ? '#000' : '#fff';
+
   return (
     <>
       <h1>Random Color Generator</h1>
       <div
         style={{
           fontSize: '50px',
-          color: hexColor,
+          color: textColor,
+          backgroundColor: hexColor,
         }}
       >
         Generated Color: {hexColor}
