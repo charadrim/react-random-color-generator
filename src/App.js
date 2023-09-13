8a6b1927161cf7774288269452a11244e2c87eb0
Chara Drimiskianaki <charadrim@yahoo.gr>
2023-09-13 09:15:51 +0300

    Finish react random color generator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import randomColor from 'randomcolor';
 import { useState } from 'react';
-import tinycolor from 'tinycolor2';
 
 export default function App() {
   const initialColor = randomColor();
-  const [hexColor, setHexColor] = useState(initialColor);
-  const textColor = tinycolor(hexColor).isLight() ? '#000' : '#fff';
-
+  const newColor = randomColor();
+  const boxSize = '400px';
+  const [color, setColor] = useState(initialColor);
   return (
-    <>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
       <h1>Random Color Generator</h1>
       <div
         style={{
-          fontSize: '50px',
-          color: textColor,
-          backgroundColor: hexColor,
+          width: boxSize,
+          height: boxSize,
+          fontSize: '30px',
+          color: '#000',
+          textShadow: '2px 2px 4px #fff',
+          backgroundColor: newColor,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        Generated Color: {hexColor}
+        Generated Color:
+        <br />
+        <div
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          {newColor}
+        </div>
       </div>
-      <br />
-      <button onClick={() => setHexColor(randomColor())}>Generate</button>
-    </>
+      <button
+        style={{ marginTop: '20px' }}
+        onClick={() => {
+          setColor(newColor);
+        }}
+      >
+        Generate
+      </button>
+    </div>
   );
 }
